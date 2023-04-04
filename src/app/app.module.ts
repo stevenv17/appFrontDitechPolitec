8fc5531ed0d01fdfddf4947888f4d6d3514de959
Carlos Steven Vargas Hernandez <steven17.v@gmail.com>
2023-04-03 20:46:57 -0500

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,84 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+//Ant
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NZ_ICONS } from 'ng-zorro-antd/icon';
+import { IconDefinition } from '@ant-design/icons-angular';
+import * as AllIcons from '@ant-design/icons-angular/icons';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { ClientsComponent } from './clients/clients.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+registerLocaleData(en);
+
+const appRoutes: Routes = [
+  { path: 'clients', component: ClientsComponent }
+];
+
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+
+
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key])
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClientsComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    //Ant
+    NzGridModule,
+    NzTableModule,
+    NzMenuModule,
+    NzIconModule,
+    NzSelectModule,
+    NzInputModule,
+    NzFormModule,
+    NzButtonModule,
+    NzPageHeaderModule,
+    NzDatePickerModule,
+    NzToolTipModule,
+    NzModalModule,
+    NzDividerModule,
+    NzInputNumberModule,
+    NzLayoutModule,
+    NzMessageModule,
+    ReactiveFormsModule
+
+  ],
+  providers: [{ provide: NZ_I18N, useValue: en_US }, { provide: NZ_ICONS, useValue: icons }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
